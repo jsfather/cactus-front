8ccb20c04b0716bb0aa5024b0c213b0cc5884cb5
jsfather <keyvanmatinrad@outlook.com>
2025-02-05 09:54:21 +0330

    new menu items were added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,9 +4,9 @@ import { Button } from '@/components/ui/button';
 
 const Navbar = () => {
   return (
-    <header className="flex h-24 items-center rounded-[50px] px-10 shadow-md">
-      <nav className="flex w-full items-center gap-4">
-        <Link href="/" className="text-xl font-black text-black">
+    <header className="flex h-24 items-center rounded-[50px] px-10 shadow-[0px_2px_8px_0px_#F0F1F2]">
+      <nav className="flex w-full items-center">
+        <Link href="/">
           <Image
             src="/images/logo.png"
             width={70}
@@ -14,11 +14,13 @@ const Navbar = () => {
             alt="Logo of the company"
           />
         </Link>
-        <div className="flex w-full flex-row-reverse gap-4">
-          <Button title="test" />
-          <Link href="/" className="text-xl font-black text-black">
-            memories
-          </Link>
+        <div className="flex w-full flex-row-reverse items-center gap-4">
+          <Button title="ورود | ثبت نام" variant="primary" />
+          <Button title="دوره‌های آموزشی" variant="link" />
+          <Button title="محصولات" variant="link" />
+          <Button title="درباره ما" variant="link" />
+          <Button title="ارتباط با ما" variant="link" />
+          <Button title="بلاگ" variant="link" />
         </div>
       </nav>
     </header>
