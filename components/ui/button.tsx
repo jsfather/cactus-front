213e359f774fce6576ff580163a39d5af3cd9e51
jsfather <keyvanmatinrad@outlook.com>
2025-02-05 09:53:34 +0330

    link variant updated

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,7 @@ const variants = {
   secondary:
     'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
   ghost: 'hover:bg-accent hover:text-accent-foreground',
-  link: 'text-primary underline-offset-4 hover:underline',
+  link: 'text-black',
 };
 
 const sizes = {
